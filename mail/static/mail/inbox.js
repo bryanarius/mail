feb9fd1e1117670b17ff27cedf916ce7e2632112
Bryan Arius <98681192+Bryanarius@users.noreply.github.com>
2023-02-23 17:56:24 -0500

    Added reply feature and finished project

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -70,6 +70,22 @@ function view_email(id) {
     });
     document.querySelector('#email-detail-view').append(btn_arch);
     
+    const btn_reply = document.createElement('button');
+    btn_reply.innerHTML ="Reply"
+    btn_reply.className ="btn btn-info"
+    btn_reply.addEventListener('click', function() {
+      compose_email();
+
+      document.querySelector('#compose-recipients').value = email.sender;
+      let subject = email.subject;
+      if(subject.split(' ',1)[0] != "Re:") {
+        subject = "Re: " + email.subject;
+      }
+      document.querySelector('#compose-subject').value = subject;
+      document.querySelector('#compose-body').value = `On ${email.timestamp} ${email.sender} wrote: ${email.body}`;
+    });
+    document.querySelector('#email-detail-view').append(btn_reply);
+
   });
 }
 
